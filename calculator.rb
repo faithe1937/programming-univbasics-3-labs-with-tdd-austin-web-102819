5a862b0934c41b11123a606c3ed2b1772b6a461b
Faith Janicek <faithe1937@gmail.com>
2019-09-17 15:08:49 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 # Add your variables here
-first_number = 6
-second_number = 4
+first_number = 6.6
+second_number = 4.4
 sum = (first_number + second_number)
 difference = (second_number - first_number)
 product = (first_number * second_number)
