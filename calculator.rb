50240e36c6f2ef8c033eece4c6bfe7367a52469f
Faith Janicek <faithe1937@gmail.com>
2019-09-17 15:05:24 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,3 +2,6 @@
 first_number = 6
 second_number = 4
 sum = (first_number + second_number)
+difference = (second_number - first_number)
+product = (first_number * second_number)
+quotient = (first_number / second_number)
